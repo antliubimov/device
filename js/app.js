32e99e97e04dce145116933addc05a3207993d47
Anton Liubimov <ant.liubimov@gmail.com>
2020-02-12 14:31:50 +0500

    done js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,39 @@
 'use strict';
 
 let btnWrite = document.querySelector('.btn__write');
-
 let modalWriteForm = document.querySelector('.modal__write_us');
-let writeFormSubmit = modalWriteForm.querySelector('.btn__submit');
-let writeFormClose = modalWriteForm.querySelector('.modal__close');
-
-btnWrite.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  modalWriteForm.classList.add('modal__show');
-})
-
 
 let infoMap = document.querySelector('.info__map');
 let modalMap = document.querySelector('.modal__map');
-let mapClose = modalMap.querySelector('.modal__close');
 
-infoMap.addEventListener('click', function (evt) {
+let popupActive = (evt) => {
   evt.preventDefault();
-  modalMap.classList.add('modal__show');
-});
+  let popup = (evt.target === btnWrite) ? modalWriteForm : modalMap;
+  let popupCloseBtn = popup.querySelector('.modal__close');
+  popup.classList.add('modal__show');
+  popupCloseBtn.addEventListener('click', popupCloseClick);
+  document.addEventListener('keydown', escPressDown);
+};
 
+let popupClose = () => {
+  let popup = document.querySelector('.modal__show');
+  let popupCloseBtn = popup.querySelector('.modal__close');
+  popup.classList.remove('modal__show');
+  popupCloseBtn.removeEventListener('click', popupCloseClick);
+  document.removeEventListener('keydown', escPressDown);
+};
 
-mapClose.addEventListener('click', function (evt) {
+let popupCloseClick = (evt) => {
   evt.preventDefault();
-  modalMap.classList.remove('modal__show');
-})
+  popupClose();
+}
+
+let escPressDown = (evt) => {
+  evt.preventDefault();
+  if (evt.key === 'Escape') {
+    popupClose();
+  }
+};
+
+btnWrite.addEventListener('click', popupActive);
+infoMap.addEventListener('click', popupActive);
