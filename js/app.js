53287d75cde508e38851f6f8d22ab6cb99fc7611
Anton Liubimov <ant.liubimov@gmail.com>
2020-02-12 11:45:39 +0500

    done styles of writePopup

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 'use strict';
 
-let btnWrite = document.querySelector('btn__write');
+let btnWrite = document.querySelector('.btn__write');
+
+let modalWriteForm = document.querySelector('.modal__write_us');
+let writeFormSubmit = modalWriteForm.querySelector('.btn__submit');
+let writeFormClose = modalWriteForm.querySelector('.modal__close');
+
+btnWrite.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalWriteForm.classList.add('modal__show');
+})
+
+
 let infoMap = document.querySelector('.info__map');
 let modalMap = document.querySelector('.modal__map');
 let mapClose = modalMap.querySelector('.modal__close');
